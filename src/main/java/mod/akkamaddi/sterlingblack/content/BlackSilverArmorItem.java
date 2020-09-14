0d391c19498501edd50661c7ebb53c57d0308401
Sinhika <sinhika@republicofnewhome.org>
2020-09-14 11:51:37 -0500

    colorized the tool tips, corrected misspelled translation key

--- src/main/java/mod/akkamaddi/sterlingblack/content/BlackSilverArmorItem.java
+++ src/main/java/mod/akkamaddi/sterlingblack/content/BlackSilverArmorItem.java
@@ -12,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
@@ -38,7 +39,7 @@ public class BlackSilverArmorItem extends ArmorItem
     public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn)
     {
         super.addInformation(stack, worldIn, tooltip, flagIn);
-        tooltip.add(new TranslationTextComponent("sterlingblack.blacksilver_armor.info"));
+        tooltip.add(new TranslationTextComponent("sterlingblack.blacksilver_armor.info").applyTextStyle(TextFormatting.GREEN));
     }
 
     
--- src/main/java/mod/akkamaddi/sterlingblack/content/SterlingSteelArmorItem.java
+++ src/main/java/mod/akkamaddi/sterlingblack/content/SterlingSteelArmorItem.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -40,7 +41,7 @@ public class SterlingSteelArmorItem extends ArmorItem
     public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn)
     {
         super.addInformation(stack, worldIn, tooltip, flagIn);
-        tooltip.add(new TranslationTextComponent("sterlingblack.sterlingsteel_armor.info"));
+        tooltip.add(new TranslationTextComponent("sterlingblack.sterlingsteel_armor.info").applyTextStyle(TextFormatting.GREEN));
     }
 
     @Override
