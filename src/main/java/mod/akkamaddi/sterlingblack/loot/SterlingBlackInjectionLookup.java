719d316927a1da257bdaa18933871aecf8a1ad98
Sinhika <sinhika@republicofnewhome.org>
2021-05-15 18:17:10 -0500

    added skeleton of loot injection lookup table

---
+++ src/main/java/mod/akkamaddi/sterlingblack/loot/SterlingBlackInjectionLookup.java
@@ -0,0 +1,13 @@
+package mod.akkamaddi.sterlingblack.loot;
+
+import mod.alexndr.simplecorelib.helpers.InjectionTableLookup;
+
+public class SterlingBlackInjectionLookup extends InjectionTableLookup
+{
+
+    public SterlingBlackInjectionLookup()
+    {
+       super();
+    } // end ctor()
+
+} // end class
